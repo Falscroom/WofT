848f67aba080ea7e55e2038f09147cac17a46638
falscroom <falscroom@gmail.com>
2015-09-28 00:35:50 +1000

    Сделано отображение нескольких событий

--- application/models/model_calendar.php
+++ application/models/model_calendar.php
@@ -6,9 +6,11 @@ Class Model_Calendar extends Model {
 /*        $arr = Array((new DateTime($result[0]["ev_date"]))->format('m-d-Y') => $result[0]["professor"]);
         return  json_encode($arr);*/
         foreach($events_array as $element_event) {
-            $text_event =  ;
+            $text_event =  '<a>'.$element_event["ev_text"].'</a>';
             $date_event = (new DateTime($element_event["ev_date"]))->format('m-d-Y');
+            $json_encoded_array[$date_event] = $text_event;
         }
+        return json_encode($json_encoded_array);
 
     }
 }
