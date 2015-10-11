9ef8b94660ac71527a58cfe7af6034d5091ef09e
Alexander Makeev <rhmbwt@gmail.com>
2015-10-11 21:03:48 +1000

    Comments

--- application/controllers/controller_book.php
+++ application/controllers/controller_book.php
@@ -1,6 +1,10 @@
 <?php
 class Controller_Book extends Controller
 {
+    function __construct() {
+        $this->view = new View();
+        $this->model = new Model_Book();
+    }
     function action_index()
     {	
         $this->view->generate('book_view.php', 'template_view.php');
--- application/controllers/controller_feedback.php
+++ application/controllers/controller_feedback.php
@@ -1,6 +1,10 @@
 <?php
 class Controller_Feedback extends Controller
 {
+    function __construct() {
+        $this->view = new View();
+        $this->model = new Model_Feedback();
+    }
     function action_index()
     {
         $this->view->generate('feedback_view.php', 'template_view.php');
