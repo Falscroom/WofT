ceb8d7a5cef889aaa398c038cef0e09c756b302b
Alexander Makeev <rhmbwt@gmail.com>
2015-11-30 13:07:18 +1000

    News 2.1

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -21,7 +21,7 @@
                <li><a href="/book">Журнал</a></li>
                <li><a href="/feedback">Обратная связь</a></li>
                <?php if ($data['login']):?>
-                   <li class="navbar-right" data-toggle="modal" data-target="#myModal"><a href="/profile/<?php echo $_COOKIE['login'] ?>" ><?  echo $_COOKIE['login']; ?></a></li>
+                   <li class="navbar-right" data-toggle="modal" data-target="#myModal"><a href="/profile/<?php echo $_COOKIE['login'] ?>" ><?php  echo $_COOKIE['login']; ?></a></li>
                <?php else: ?>
                    <li class="navbar-right"><a href="/login" data-toggle="modal" data-target="#myModal">Вход</a></li>
                <?php endif; ?>
